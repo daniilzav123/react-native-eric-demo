2332784f2cd7ee0df49b4744767ba9569ad72913
Daniil Zavyalov <daniilzavup@gmail.com>
2017-08-19 11:52:44 +0200

    added reselect package

---
+++ src/colors/index.js
@@ -0,0 +1,19 @@
+/**
+ * @providesModule AppColors
+ **/
+
+export const WHITE = "#f7f7f7";
+export const LIGHTGRAY_COLOR = "#f4f4f4";
+export const GRAY_COLOR = "#b6b4b3";
+export const DARK_COLOR = "#3c3c3c";
+export const ORANGE_COLOR = "#ec6e00";
+export const BLUE_COLOR = "#0044ce";
+export const WHITEGRAY_COLOR = "#ececec";
+export const GREY_COLOR = "#d7d6d6";
+export const GREY_COLOR1 = "#d2d2d2";
+export const DARKGRAY_COLOR = "#7b7a7a";
+export const BLACK_COLOR = "#111111";
+export const DARKBLACK_COLOR = "#2f2f2f";
+export const YELLOW_COLOR = "#ffba00";
+export const GRAY1_COLOR = "#dedede";
+export const GRAY_COLOR2 = "#dad9d9";
